913ffb88f182508c61fcad02b8589e4095ed4950
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-03-27 13:58:05 +0100

    removed every thing.

--- app/src/main/java/no/kristiania/android/contentproviderexample/provider/StudentContentProvider.kt
+++ app/src/main/java/no/kristiania/android/contentproviderexample/provider/StudentContentProvider.kt
@@ -1,6 +1,9 @@
 package no.kristiania.android.contentproviderexample.provider
 
+import android.content.ContentProvider
+import android.content.ContentValues
 import android.content.UriMatcher
+import android.database.Cursor
 import android.net.Uri
 import no.kristiania.android.contentproviderexample.db.StudentDAO
 
@@ -14,19 +17,65 @@ import no.kristiania.android.contentproviderexample.db.StudentDAO
  * Date: 26 March 2020
  */
 
-class StudentContentProvider {
+class StudentContentProvider : ContentProvider() {
 
     private lateinit var studentDao: StudentDAO
 
     companion object {
         val AUTHORITY: String =
             "no.kristiania.android.contentproviderexample.provider.StudentContentProvider"
+
         val CONTETNT_URI: Uri = Uri.parse("content://${AUTHORITY}/STUDENT_TABLE")
 
         val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
             addURI(AUTHORITY, "STUDENT_TABLE", 1)
             addURI(AUTHORITY, "STUDENT_TABLE/#", 2)
         }
+
+
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        val id = studentDao.insert(values!!)
+        return Uri.parse(id.toString())
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        val type = uriMatcher.match(uri)
+        return if (type == 1) {
+            studentDao.fetchAllRecodeAsCursor()
+        } else {
+            null
+        }
+
+    }
+
+    override fun onCreate(): Boolean {
+        studentDao = StudentDAO(context!!)
+        return true
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getType(uri: Uri): String? {
+        TODO("Not yet implemented")
     }
 
 
--- app/src/main/java/no/kristiania/android/contentproviderexample/ui/MainActivity.kt
+++ app/src/main/java/no/kristiania/android/contentproviderexample/ui/MainActivity.kt
@@ -1,26 +1,33 @@
 package no.kristiania.android.contentproviderexample.ui
 
+import android.content.ContentValues
+import android.database.Cursor
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import no.kristiania.android.contentproviderexample.R
+import no.kristiania.android.contentproviderexample.Student
+import no.kristiania.android.contentproviderexample.db.StudentTable
+import no.kristiania.android.contentproviderexample.provider.StudentContentProvider
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
 
 
     private lateinit var loaderManagerInstance: LoaderManager
     private lateinit var adapter: StudentAdapter
 
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loaderManagerInstance = LoaderManager.getInstance(this)
 
         adapter = StudentAdapter(
             this,
@@ -45,7 +52,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initListData() {
-
+        loaderManagerInstance.initLoader(11, Bundle.EMPTY, this)
     }
 
     private fun refreshList() {
@@ -53,7 +60,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addData() {
+        val value = ContentValues()
+        value.put(StudentTable.COLUMN_NAME, edit_name.text.toString())
+        val uri = contentResolver
+            .insert(StudentContentProvider.CONTETNT_URI, value)
 
+        Toast.makeText(this, uri.toString(), Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
@@ -64,5 +76,35 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         // Do close the db after use.
         //studentDAO.close()
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return CursorLoader(
+            this,
+            StudentContentProvider.CONTETNT_URI,
+            arrayOf(StudentTable.COLUMN_ID, StudentTable.COLUMN_NAME),
+            null,
+            null,
+            null
+        )
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, cursor: Cursor) {
+        val list = mutableListOf<Student>()
+
+        while (cursor.moveToNext()) {
+            list.add(
+                Student(
+                    cursor.getLong(cursor.getColumnIndex(StudentTable.COLUMN_ID)),
+                    cursor.getString(cursor.getColumnIndex(StudentTable.COLUMN_NAME))
+                )
+            )
+        }
+
+        adapter.setData(list)
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        adapter.setData(mutableListOf())
     }
 }
