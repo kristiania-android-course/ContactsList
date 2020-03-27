bd3cab435f124e22d546848f10610db508a820a8
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-03-27 08:51:47 +0100

    removed every thing.

--- app/src/main/java/no/kristiania/android/contentproviderexample/ui/ContactsActivity.kt
+++ app/src/main/java/no/kristiania/android/contentproviderexample/ui/ContactsActivity.kt
@@ -11,46 +11,14 @@ import androidx.loader.content.Loader
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_contacts.*
 import no.kristiania.android.contentproviderexample.R
-
+                                    /*ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME ContactsContract.Contacts.CONTENT_URI*/
 
 class ContactsActivity : AppCompatActivity() {
 
-    // Defines the id of the loader for later reference
-    private val CONTACT_LOADER_ID = 78 // From docs: A unique identifier for this loader. Can be whatever you want.
-
-
     private lateinit var adapter: ContactAdapter
 
-    // Defines the asynchronous callback for the contacts data loader
-    private val contactsLoader: LoaderManager.LoaderCallbacks<Cursor> = object : LoaderManager.LoaderCallbacks<Cursor> {
-        override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
 
-            val projectionFields = arrayOf(ContactsContract.Contacts._ID,
-                    ContactsContract.Contacts.DISPLAY_NAME)
 
-            return CursorLoader(this@ContactsActivity,
-                    ContactsContract.Contacts.CONTENT_URI,  // URI
-                    projectionFields,  // projection fields
-                    null,  // the selection criteria
-                    null,  // the selection args
-                    null // the sort order
-            )
-        }
-
-        override fun onLoadFinished(loader: Loader<Cursor>, cursor: Cursor) {
-            // The swapCursor() method assigns the new Cursor to the adapter
-            val list = mutableListOf<String>()
-            while (cursor.moveToNext()){
-                list.add(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)))
-            }
-            adapter.setData(list)
-        }
-
-        override fun onLoaderReset(loader: Loader<Cursor>) {
-            // Clear the Cursor we were using with another call to the swapCursor()
-            adapter.setData(mutableListOf());
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,19 +30,8 @@ class ContactsActivity : AppCompatActivity() {
         ) {
             Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         }
-        contacts_recycler_view.layoutManager = GridLayoutManager(this, 1)
+        contacts_recycler_view.layoutManager = GridLayoutManager(this, 2)
         contacts_recycler_view.adapter = adapter
-
-        // Initialize the loader with a special ID and the defined callbacks from above
-        // Initialize the loader with a special ID and the defined callbacks from above
-        LoaderManager
-                .getInstance(this)
-                .initLoader(
-                        CONTACT_LOADER_ID,
-                        Bundle(),
-                        contactsLoader
-                )
-
 
     }
 
