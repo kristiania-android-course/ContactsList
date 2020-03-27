ab3dc3c7406105151cc390f0961b222fa95b2780
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-03-27 10:37:03 +0100

    removed every thing.

--- app/src/main/java/no/kristiania/android/contentproviderexample/provider/StudentContentProvider.kt
+++ app/src/main/java/no/kristiania/android/contentproviderexample/provider/StudentContentProvider.kt
@@ -1,12 +1,8 @@
 package no.kristiania.android.contentproviderexample.provider
 
-import android.content.ContentProvider
-import android.content.ContentValues
 import android.content.UriMatcher
-import android.database.Cursor
 import android.net.Uri
 import no.kristiania.android.contentproviderexample.db.StudentDAO
-import java.lang.IllegalArgumentException
 
 
 /**
@@ -18,90 +14,20 @@ import java.lang.IllegalArgumentException
  * Date: 26 March 2020
  */
 
-class StudentContentProvider : ContentProvider(){
+class StudentContentProvider {
 
-    private lateinit var studentDAO: StudentDAO
-
-
-    private val Get_All_Data = 1
-    private val Get_Single_Data: Int = 2
-
+    private lateinit var studentDao: StudentDAO
 
     companion object {
-        private const val AUTHORITY = "no.kristiania.android.contentproviderexample.provider.StudentContentProvider"
-        val URL = "content://$AUTHORITY/STUDENT_TABLE"
-        val CONTENT_URI = Uri.parse(URL)
-    }
+        val AUTHORITY: String =
+            "no.kristiania.android.contentproviderexample.provider.StudentContentProvider"
+        val CONTETNT_URI: Uri = Uri.parse("content://${AUTHORITY}/STUDENT_TABLE")
 
-    private val sUriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
-        /*
-         * The calls to addURI() go here, for all of the content URI patterns that the provider
-         * should recognize. For this snippet, only the calls for table 3 are shown.
-         */
-
-        /*
-         * Sets the integer value for multiple rows in STUDENT_TABLE to 1. Notice that no wildcard is used
-         * in the path
-         */
-        addURI("no.kristiania.android.contentproviderexample.provider.StudentContentProvider", "STUDENT_TABLE", Get_All_Data)
-        //addURI("no.kristiania.android.contentproviderexample.provider.StudentContentProvider", "STUDENT_TABLE/#", Get_Single_Data)
-
-
-    }
-
-    override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        val id =  values?.let {
-            studentDAO.insert(it)
-        } ?: kotlin.run {
-            throw IllegalArgumentException("No ContentValues available")
-        }
-        return Uri.parse("STUDENT_TABLE/$id")
-    }
-
-    override fun query(
-        uri: Uri,
-        projection: Array<out String>?,
-        selection: String?,
-        selectionArgs: Array<out String>?,
-        sortOrder: String?
-    ): Cursor? {
-        val type = sUriMatcher.match(uri)
-
-        return when (type) {
-            Get_All_Data -> {
-                studentDAO.fetchAllRecodeAsCursor()
-            }
-            Get_Single_Data -> {
-                uri.lastPathSegment?.toLong()?.let {
-                    studentDAO.getRecordWithID(it)
-                }
-            }
-            else -> {
-                null
-            }
+        val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+            addURI(AUTHORITY, "STUDENT_TABLE", 1)
+            addURI(AUTHORITY, "STUDENT_TABLE/#", 2)
         }
     }
 
-    override fun onCreate(): Boolean {
-        studentDAO = StudentDAO(context!!)
-        return true
-    }
-
-    override fun update(
-        uri: Uri,
-        values: ContentValues?,
-        selection: String?,
-        selectionArgs: Array<out String>?
-    ): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun getType(uri: Uri): String? {
-        TODO("Not yet implemented")
-    }
 
 }
--- app/src/main/java/no/kristiania/android/contentproviderexample/ui/MainActivity.kt
+++ app/src/main/java/no/kristiania/android/contentproviderexample/ui/MainActivity.kt
@@ -1,20 +1,12 @@
 package no.kristiania.android.contentproviderexample.ui
 
-import android.content.ContentValues
-import android.database.Cursor
-import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.loader.app.LoaderManager
-import androidx.loader.content.CursorLoader
-import androidx.loader.content.Loader
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import no.kristiania.android.contentproviderexample.R
-import no.kristiania.android.contentproviderexample.Student
-import no.kristiania.android.contentproviderexample.db.StudentTable
-import no.kristiania.android.contentproviderexample.provider.StudentContentProvider
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,47 +14,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var loaderManagerInstance: LoaderManager
     private lateinit var adapter: StudentAdapter
-    private val studentLoader: LoaderManager.LoaderCallbacks<Cursor> = object : LoaderManager.LoaderCallbacks<Cursor> {
-        override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
 
-            val projectionFields = arrayOf(
-                StudentTable.COLUMN_ID,
-                StudentTable.COLUMN_NAME)
 
-            return CursorLoader(this@MainActivity,
-                StudentContentProvider.CONTENT_URI,  // URI
-                projectionFields,  // projection fields
-                null,  // the selection criteria
-                null,  // the selection args
-                null // the sort order
-            )
-        }
 
-        override fun onLoadFinished(loader: Loader<Cursor>, cursor: Cursor) {
-            // The swapCursor() method assigns the new Cursor to the adapter
-            val list = mutableListOf<Student>()
-            while (cursor.moveToNext()){
-                list.add(
-                    Student(
-                        cursor.getLong(cursor.getColumnIndex(StudentTable.COLUMN_ID)),
-                        cursor.getString(cursor.getColumnIndex(StudentTable.COLUMN_NAME))
-                    )
-                )
-            }
-            adapter.setData(list)
-        }
-
-        override fun onLoaderReset(loader: Loader<Cursor>) {
-            // Clear the Cursor we were using with another call to the swapCursor()
-            adapter.setData(mutableListOf());
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        loaderManagerInstance = LoaderManager.getInstance(this)
 
         adapter = StudentAdapter(
             this,
@@ -77,33 +35,24 @@ class MainActivity : AppCompatActivity() {
 
         // OnClick listener to button
         btn_save.setOnClickListener {
-
-            val values = ContentValues()
-            values.put(
-                StudentTable.COLUMN_NAME,
-                edit_name.text.toString()
-            )
-
-            val uri: Uri? = contentResolver.insert(
-                StudentContentProvider.CONTENT_URI, values
-            )
-
-            Toast.makeText(getBaseContext(),
-                uri.toString(), Toast.LENGTH_LONG).show();
-
-            loaderManagerInstance.restartLoader(222, Bundle.EMPTY, studentLoader)
-
-            /*// Get value from the edit text (edit_name)
-            val name = edit_name.text.toString()
-            // Save it to DB
-            val id = studentDAO.insert(name)
-            // Show what is stored
-            Toast.makeText(this, "stored $name in row $id ", Toast.LENGTH_SHORT).show()
-*/
+            addData()
+            refreshList()
         }
 
-        loaderManagerInstance.initLoader(11, Bundle.EMPTY, studentLoader)
+        initListData()
 
+
+    }
+
+    private fun initListData() {
+
+    }
+
+    private fun refreshList() {
+
+    }
+
+    private fun addData() {
 
     }
 
